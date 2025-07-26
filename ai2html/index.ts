42b76440c3538b14aae2329894e97b604571a43f
Max Kohler <hello@maxkohler.com>
2025-07-26 19:49:13 +0200

    Drop unused function

--- ai2html/index.ts
+++ ai2html/index.ts
@@ -690,26 +690,6 @@ function main() {
 		return o
 	}
 
-	// TODO: improve
-	// (currently ignores bracketed sections of the config file)
-	function readGitConfigFile(path) {
-		var file = new File(path)
-		var o = null
-		var parts
-		if (file.exists) {
-			o = {}
-			file.open("r")
-			while (!file.eof) {
-				parts = file.readln().split("=")
-				if (parts.length > 1) {
-					o[trim(parts[0])] = trim(parts[1])
-				}
-			}
-			file.close()
-		}
-		return o
-	}
-
 	function readFile(fpath, enc) {
 		var content = null
 		var file = new File(fpath)
