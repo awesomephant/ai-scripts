e10321e9058021fc85932e2014ea7105ade35074
Max Kohler <hello@maxkohler.com>
2025-07-31 00:19:26 +0200

    Add missing import

--- test/extendFontlist.test.ts
+++ test/extendFontlist.test.ts
@@ -1,41 +1,47 @@
-import extendFontList from "../ai2html/extendFontlist";
+import { it, expect } from "vitest"
+import extendFontList from "../ai2html/extendFontlist"
 
-const fonts = [{
-    aifont: "ArialMT",
-    family: "arial,helvetica,sans-serif",
-    weight: "",
-    style: ""
-},
-{
-    aifont: "Arial-BoldMT",
-    family: "arial,helvetica,sans-serif",
-    weight: "bold",
-    style: ""
-}]
+const fonts = [
+	{
+		aifont: "ArialMT",
+		family: "arial,helvetica,sans-serif",
+		weight: "",
+		style: ""
+	},
+	{
+		aifont: "Arial-BoldMT",
+		family: "arial,helvetica,sans-serif",
+		weight: "bold",
+		style: ""
+	}
+]
 it("adds a new FontRule to the list", () => {
-    const additional = {
-        aifont: "Georgia",
-        family: "georgia,'times new roman'",
-        weight: "",
-        style: ""
-    }
-    const t = extendFontList(fonts, [additional])
-    expect(t).toStrictEqual([...fonts, ...[additional]])
-    expect
+	const additional = {
+		aifont: "Georgia",
+		family: "georgia,'times new roman'",
+		weight: "",
+		style: ""
+	}
+	const t = extendFontList(fonts, [additional])
+	expect(t).toStrictEqual([...fonts, ...[additional]])
+	expect
 })
 
 it("overwrites if a.aifont matches b.aifont", () => {
-    const additional = {
-        aifont: "ArialMT",
-        family: "Test",
-        weight: "",
-        style: ""
-    }
-    const t = extendFontList(fonts, [additional])
-    expect(t).toStrictEqual([...[additional], {
-        aifont: "Arial-BoldMT",
-        family: "arial,helvetica,sans-serif",
-        weight: "bold",
-        style: ""
-    }])
+	const additional = {
+		aifont: "ArialMT",
+		family: "Test",
+		weight: "",
+		style: ""
+	}
+	const t = extendFontList(fonts, [additional])
+	expect(t).toStrictEqual([
+		...[additional],
+		{
+			aifont: "Arial-BoldMT",
+			family: "arial,helvetica,sans-serif",
+			weight: "bold",
+			style: ""
+		}
+	])
 })
