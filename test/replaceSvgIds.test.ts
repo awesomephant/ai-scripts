e5cb02934407abf07c034dd73fceb34ae8b9a2a4
Max Kohler <hello@maxkohler.com>
2025-07-31 14:21:29 +0200

    Extract replaceSvgIds()

--- test/replaceSvgIds.test.ts
+++ test/replaceSvgIds.test.ts
@@ -2,35 +2,18 @@ import { it, expect, vi } from "vitest"
 import replaceSvgIds from "../common/replaceSvgIds"
 
 it("persist known id list", () => {
-	let knownIds: string[] = [] // index of ids
-	let { svg, ids } = replaceSvgIds('id="dot_1_"', "", knownIds)
-
-	expect({ svg, ids }).toStrictEqual({
-		svg: 'id="dot" data-name="dot"',
-		ids: ["dot"]
-	})
-
-	knownIds = ids
-	expect(replaceSvgIds('id="dot_1_"', "", knownIds)).toStrictEqual({
-		svg: 'id="dot-2" data-name="dot"',
-		ids: ["dot", "dot-2"]
-	})
+	let svg = replaceSvgIds('id="dot_1_"', "")
+	expect(svg).toBe('id="dot" data-name="dot"')
 })
 
 it("replaces hex codes", () => {
-	expect(replaceSvgIds('id="_x5F_a_x5F_b_x5F__2_"')).toStrictEqual({
-		svg: 'id="_a_b_" data-name="_a_b_"',
-		ids: ["_a_b_"]
-	})
+	expect(replaceSvgIds('id="_x5F_a_x5F_b_x5F__2_"')).toBe('id="_a_b_" data-name="_a_b_"')
 })
 
 it("warns on duplicate", () => {
 	const ondupe = vi.fn()
-	const res = replaceSvgIds('id="rect_4_" id="rect_8_"', "", [], ondupe)
-	expect(res).toStrictEqual({
-		svg: 'id="rect" data-name="rect" id="rect-2" data-name="rect"',
-		ids: ["rect", "rect-2"]
-	})
+	const res = replaceSvgIds('id="rect_4_" id="rect_8_"', "", ondupe)
+	expect(res).toBe('id="rect" data-name="rect" id="rect-2" data-name="rect"')
 	expect(ondupe).toHaveBeenCalled()
 	expect(ondupe).toHaveBeenCalledWith(["rect"])
 })
